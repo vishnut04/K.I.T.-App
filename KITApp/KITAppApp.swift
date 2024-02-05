a8d24f5de585c45622741853b913e56dc2c1a7b2
vishnut04 <vtunuguntla022@gmail.com>
2024-02-04 19:16:58 -0500

    Initial Commit

---
+++ KITApp/KITAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KITAppApp.swift
+//  KITApp
+//
+//  Created by Vishnu Tunuguntla on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct KITAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
